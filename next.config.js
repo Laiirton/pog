4f9dd98fcc78d798a94b8003bf350c15cb8a2413
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 03:02:03 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,6 +17,27 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://pog-git-master-lairtons-projects.vercel.app, https://pog-five.vercel.app, https://pog-a1877eeot-lairtons-projects.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
