ec0150dd2c09111aabf138f3b66a3ae6e55c9313
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 04:27:29 -0300

    Update route.ts

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,4 +1,4 @@
-import type { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 import { google } from 'googleapis';
 import { Readable } from 'stream';
