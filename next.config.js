f40a0f686bf9457f0e0ff12e130029dc1910cac1
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 02:32:39 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'pog-five.vercel.app'].filter(Boolean),
+    domains: ['localhost', 'pog-five.vercel.app', 'tycfnjqspnvgyhjsjvfs.supabase.co'],
   },
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,11 +9,13 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  rewrites: async () => {
+  async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://pog-five.vercel.app/api/:path*',
+        destination: process.env.NODE_ENV === 'production'
+          ? 'https://pog-five.vercel.app/api/:path*'
+          : 'http://localhost:3001/api/:path*',
       },
     ];
   },
