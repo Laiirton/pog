7bdc0ae0c9c9eadbeb065697dbf6d44379c7b480
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 04:27:18 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,46 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: ['localhost', 'pog-five.vercel.app', 'tycfnjqspnvgyhjsjvfs.supabase.co', 'pog-git-master-lairtons-projects.vercel.app'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/api/:path*',
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://pog-git-master-lairtons-projects.vercel.app, https://pog-five.vercel.app, https://pog-a1877eeot-lairtons-projects.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,DELETE,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
-  output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: undefined,
+    domains: ['lh3.googleusercontent.com'],
   },
 }
 
