55fc44c83a29b3354a826ef7a204fe1e88ce7cf5
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 04:27:36 -0300

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,4 +1,4 @@
-import type { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
