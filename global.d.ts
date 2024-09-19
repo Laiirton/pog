735af33cf17e39bf1ac578c1a71d46fe5772df35
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 02:13:37 -0300

    Create global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,21 @@
+declare module 'fluent-ffmpeg' {
+  const ffmpeg: any;
+  export default ffmpeg;
+}
+
+declare module '@localnerve/google-drive-folder' {
+  const googleDriveFolder: any;
+  export default googleDriveFolder;
+}
+
+declare module 'jsonwebtoken' {
+  const jwt: any;
+  export default jwt;
+}
+
+declare module 'body-parser' {
+  const bodyParser: any;
+  export default bodyParser;
+}
+
+// Adicione outras declarações conforme necessário
