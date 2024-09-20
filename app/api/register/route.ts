b55589f501538109fa8c410042cd36ba6ef44d51
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 21:31:32 -0300

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -30,7 +30,7 @@ export async function POST(req: NextRequest) {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Inserir novo usuário
-    const { data, error } = await supabase
+    const { error } = await supabase
       .from('usernames')
       .insert([
         { username, password: hashedPassword, upload_count: 0 }
