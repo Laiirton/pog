fb529b5b3991e883b1fbe667780b4140759751c7
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 21:49:22 -0300

    Update loading-animation.tsx

--- components/loading-animation.tsx
+++ components/loading-animation.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import { motion } from 'framer-motion';
+
 
 export const LoadingAnimation: React.FC = () => {
   const containerVariants = {
