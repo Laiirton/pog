82b082485e07e01343b4985450ce25d11351832b
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 22:43:36 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['drive.google.com', 'lh3.googleusercontent.com'],
+    unoptimized: true,
+  },
   env: {
     GOOGLE_DRIVE_CLIENT_ID: process.env.GOOGLE_DRIVE_CLIENT_ID,
     GOOGLE_DRIVE_CLIENT_SECRET: process.env.GOOGLE_DRIVE_CLIENT_SECRET,
@@ -7,13 +11,6 @@ const nextConfig = {
     GOOGLE_DRIVE_REFRESH_TOKEN: process.env.GOOGLE_DRIVE_REFRESH_TOKEN,
     GOOGLE_DRIVE_FOLDER_ID: process.env.GOOGLE_DRIVE_FOLDER_ID,
   },
-  images: {
-    domains: ['drive.google.com', 'lh3.googleusercontent.com'],
-  },
-  serverRuntimeConfig: {
-    // Adicione isso para usar fs no servidor
-    fs: 'empty'
-  },
 }
 
-export default nextConfig;
+module.exports = nextConfig
