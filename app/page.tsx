3344ac511792b19dafcd02615c3523ffa874447f
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 21:02:04 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { RetroMediaGalleryComponent } from '../components/retro-media-gallery';
 import { AuthScreen } from '../components/auth-screen';
 import { useState, useEffect } from 'react';
-import { LoadingAnimation } from '../components/retro-media-gallery';
+import { LoadingAnimation } from '../components/loading-animation';
 
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
