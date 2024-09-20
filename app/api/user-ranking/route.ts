1f58617951a1add148dbbe9e76ec6441f4328350
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-19 21:18:28 -0300

    fix deploy

--- app/api/user-ranking/route.ts
+++ app/api/user-ranking/route.ts
@@ -6,7 +6,7 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const { data, error } = await supabase
       .from('usernames')
