4c8339fea4e34b28b5ee1ce8621ced75136a07b9
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-09-20 17:25:30 -0300

    Update route.ts

--- app/api/file/[id]/route.ts
+++ app/api/file/[id]/route.ts
@@ -3,18 +3,22 @@ import { google } from 'googleapis';
 import { OAuth2Client } from 'google-auth-library';
 import { Readable } from 'stream';
 
+// Configuração do cliente OAuth2 para autenticação com o Google Drive
 const oauth2Client = new OAuth2Client(
   process.env.GOOGLE_DRIVE_CLIENT_ID,
   process.env.GOOGLE_DRIVE_CLIENT_SECRET,
   process.env.GOOGLE_DRIVE_REDIRECT_URI
 );
 
+// Configuração das credenciais usando o refresh token
 oauth2Client.setCredentials({
   refresh_token: process.env.GOOGLE_DRIVE_REFRESH_TOKEN,
 });
 
+// Inicialização da API do Google Drive
 const drive = google.drive({ version: 'v3', auth: oauth2Client });
 
+// Função para lidar com requisições GET
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
@@ -22,11 +26,13 @@ export async function GET(
   const fileId = params.id;
 
   try {
+    // Busca o arquivo no Google Drive
     const file = await drive.files.get({
       fileId: fileId,
       alt: 'media',
     }, { responseType: 'stream' });
 
+    // Cria um objeto Headers com os cabeçalhos da resposta do Google Drive
     const headers = new Headers();
     Object.entries(file.headers).forEach(([key, value]) => {
       if (typeof value === 'string') {
@@ -34,7 +40,7 @@ export async function GET(
       }
     });
 
-    // Converter Readable para ReadableStream
+    // Converte o Readable do Node.js para um ReadableStream da Web
     const readable = file.data as Readable;
     const stream = new ReadableStream({
       start(controller) {
@@ -44,10 +50,12 @@ export async function GET(
       },
     });
 
+    // Retorna a resposta com o stream do arquivo e os cabeçalhos
     return new NextResponse(stream, {
       headers: headers,
     });
   } catch (error) {
+    // Em caso de erro, registra o erro e retorna uma resposta de erro
     console.error('Error fetching file:', error);
     return new NextResponse('Error fetching file', { status: 500 });
   }
