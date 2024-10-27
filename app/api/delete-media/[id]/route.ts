cda67906765bc3bcaecfc41fdf38c45fc8b707a7
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-10-27 03:03:43 -0300

    Update route.ts

--- app/api/delete-media/[id]/route.ts
+++ app/api/delete-media/[id]/route.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+
 import { NextRequest, NextResponse } from 'next/server';
 import { google } from 'googleapis';
 import jwt from 'jsonwebtoken';
